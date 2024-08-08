9abbc1c708a1156d1cecf3a84dea2b5d44de99d0
Leonardo <leonardotadeu.carvalho@gmail.com>
2024-08-08 17:18:33 -0300

    create structure

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+export function Header() {
+    return (
+        <header className="w-full h-24 bg-gray-900">
+            <ul>
+                <li>Cadastrar Turma</li>
+                <li>Cadastrar Aluno</li>
+                <li>Chamada de Presença</li>
+            </ul>
+        </header>
+    )
+}
